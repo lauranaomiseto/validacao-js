6eefde1428dd6894f929c8e2825edd01680df220
Laura Naomi Seto <47746453+lauranaomiseto@users.noreply.github.com>
2023-04-09 12:01:34 -0300

    Delete assincrono directory

--- assincrono/promises-fetch.js
+++
@@ -1,6 +0,0 @@
-fetch('https://api.github.com/users/lauranaomiseto') //o fetch gera uma promessa
-.then(response => response.json()) //transformando a resposta em json, pra verificar todos os dados enviados pela api. retorna outra promessa.
-.then(data => fetch(data.repos_url)) //trata da promessa anterior e faz uma fetch, que por sua vez, retorna outra pormessa.
-.then(res => res.json()) //transforma a resposta da promessa anterior em json. retorna outra promessa.
-.then(d => console.log(d)) //trata da promessa anterior,
-.catch(erro => console.log(erro));
